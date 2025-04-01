38910872b3cc5e2f9d681d3dafdecfc5fce03b02
maulinaayu <150534522+ayuputrr@users.noreply.github.com>
2025-04-01 21:50:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-import jsonServer from 'json-server';
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = (req, res) => {
-  server(req, res);
-};
+module.exports = server;
